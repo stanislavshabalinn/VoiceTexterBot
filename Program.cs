38f3168c5938f6eac7693a7c3566557ecfcedbf9
stanislavshabalinn <shabalin_sa1@mail.ru>
2024-10-21 22:50:44 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Telegram.Bot;
+
+namespace VoiceTexterBot
+{
+    class Bot
+    {
+        private ITelegramBotClient _telegramClient;
+        public Bot(ITelegramBotClient telegramClient) 
+        { _telegramClient = telegramClient;
+        
+        }
+    }
+}
