c440a68906d2a04cbdff20efda4e1d5281385bf2
stanislavshabalinn <shabalin_sa1@mail.ru>
2024-10-27 12:11:20 +0700

    1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Telegram.Bot;
+﻿using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
@@ -7,53 +8,55 @@ using Telegram.Bot.Types.Enums;
 namespace VoiceTexterBot
 {
     // Создаем основной класс Bot
-    
+
     class Bot
     {
-         // Создаем конструктор объекта ITelegramBotClient, через который будет общение с API
-        
         private ITelegramBotClient _telegramClient;
+
         public Bot(ITelegramBotClient telegramClient)
         {
-            // Добавим метод HandleUpdateAsync (обработчик событий)
-
             _telegramClient = telegramClient;
-            async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
-            {
-                //  Обрабатываем нажатия на кнопки  из Telegram Bot API: https://core.telegram.org/bots/api#callbackquery
-                if (update.Type == UpdateType.CallbackQuery)
-                {
-                    await _telegramClient.SendTextMessageAsync(update.Message.Chat.Id, "Вы нажали кнопку", cancellationToken: cancellationToken);
-                    return;
-                }
-
-                // Обрабатываем входящие сообщения из Telegram Bot API: https://core.telegram.org/bots/api#message
-                if (update.Type == UpdateType.Message)
-                {
-                    await _telegramClient.SendTextMessageAsync(update.Message.Chat.Id, "Вы отправили сообщение", cancellationToken: cancellationToken);
-                    return;
-                }
-            }
         }
-        // Обработчик ошибок
-        Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+
+        // Обработчик событий
+
+        async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
-            // Задаем сообщение об ошибке в зависимости от того, какая именно ошибка произошла
-            var errorMessage = exception switch
+            //  Обрабатываем нажатия на кнопки  из Telegram Bot API: https://core.telegram.org/bots/api#callbackquery
+            if (update.Type == UpdateType.CallbackQuery)
             {
-                ApiRequestException apiRequestException
-                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
-                _ => exception.ToString()
-            };
+                await _telegramClient.SendTextMessageAsync(update.Message.Chat.Id, "Вы нажали кнопку", cancellationToken: cancellationToken);
+                return;
+            }
 
-            // Выводим в консоль информацию об ошибке
-            Console.WriteLine(errorMessage);
+            // Обрабатываем входящие сообщения из Telegram Bot API: https://core.telegram.org/bots/api#message
+            if (update.Type == UpdateType.Message)
+            {
+                await _telegramClient.SendTextMessageAsync(update.Message.Chat.Id, "Вы отправили сообщение", cancellationToken: cancellationToken);
+                return;
+            }
 
-            // Задержка перед повторным подключением
-            Console.WriteLine("Ожидаем 10 секунд перед повторным подключением.");
-            Thread.Sleep(10000);
+            // Метод обработки ошибок
 
-            return Task.CompletedTask;
+            Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+            {
+                // Задаем сообщение об ошибке в зависимости от того, какая именно ошибка произошла
+                var errorMessage = exception switch
+                {
+                    ApiRequestException apiRequestException
+                        => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                    _ => exception.ToString()
+                };
+
+                // Выводим в консоль информацию об ошибке
+                Console.WriteLine(errorMessage);
+
+                // Задержка перед повторным подключением
+                Console.WriteLine("Ожидаем 10 секунд перед повторным подключением.");
+                Thread.Sleep(10000);
+
+                return Task.CompletedTask;
+            }
         }
     }
 }
